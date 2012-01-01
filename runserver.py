1fd977701d19a63b1c8457a00c4599ca4b4aa8ac
(cdep) illabout <cdep.illabout@gmail.com>
2012-01-01 19:57:39 +0900

    Updated runserver.py to use the config.

--- runserver.py
+++ runserver.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python2
 import sys
 from fitto import app
+import fitto.settings
 
 host='0.0.0.0'
-debug=False
+debug=None
 
 if '-d' in sys.argv or '--debug' in sys.argv:
     debug=True
 
-app.run(debug=debug, host=host)
+if '-n' in sys.argv or '--no-debug' in sys.argv:
+    debug=False
+
+app.config.from_object('fitto.settings')
+
+if debug is not None:
+    app.run(debug=debug, host=host)
+else:
+    app.run(host=host)
